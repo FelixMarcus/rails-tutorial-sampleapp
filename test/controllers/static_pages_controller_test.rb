05850259c5aaa4a7e09b55c18d5c15b91e85db92
Felix Millne <felix.millne@btinternet.com>
2015-03-19 13:00:46 +0000

    Make small modifications

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,41 +9,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-<<<<<<< HEAD
     assert_select "title", "#{@base_title}"
-=======
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
->>>>>>> filling-in-layout
   end
 
   test "should get help" do
     get :help
     assert_response :success
-<<<<<<< HEAD
-        assert_select "title", "Help | #{@base_title}"
-
-=======
     assert_select "title", "Help | #{@base_title}"
->>>>>>> filling-in-layout
   end
 
   test "should get about" do
     get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
-<<<<<<< HEAD
-
-=======
->>>>>>> filling-in-layout
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
-<<<<<<< HEAD
-
-=======
->>>>>>> filling-in-layout
   end
 end
